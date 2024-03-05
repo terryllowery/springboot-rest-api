9eaaacd6fd051b3fc14ed525af68301f3a5b4852
terryllowery <tlowe33@my.wgu.edu>
2024-03-05 13:01:08 -0600

    added fluff

--- src/main/java/net/lowerytech/springbootrestapi/controllers/HelloWorldController.java
+++ src/main/java/net/lowerytech/springbootrestapi/controllers/HelloWorldController.java
@@ -4,12 +4,19 @@ package net.lowerytech.springbootrestapi.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @RestController
 public class HelloWorldController {
 
 
     @GetMapping("/")
-    public String hello() {
+    public String hello() throws IOException {
+        URL url = new URL("http://localhost:8080/stupid");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        System.out.println(con.getContent().toString());
         return "hello";
     }
 }
